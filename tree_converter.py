ecbe882864f7d79d6a8a8ff4bd927f8d5fe00060
ANYA KORNEYEVA <anya.korneyeva@gmail.com>
2014-03-29 11:33:47 -0400

    tree_converter: still getting "dictionary changed in size"

--- tree_converter.py
+++ tree_converter.py
@@ -7,7 +7,7 @@ from file_reader import *
 
 punct=set(['-LRB-','-RRB-','``','`','_','__','-','--','"'])
 
-fake_dict={}
+"""fake_dict={}
 sent_dict={}
 entity_dict={}
 entity_dict2={}
@@ -23,7 +23,7 @@ entity_dict2[(31,34)]='ORG'
 sent_dict[3]=entity_dict
 sent_dict[4]=entity_dict2
 
-fake_dict['APW20001007.0339.0149']=sent_dict
+fake_dict['APW20001007.0339.0149']=sent_dict"""
 
 test_tree=Tree("(ROOT (S (NP (NP (NP (NNP WASHINGTON)) (PRN (-LRB- -LRB-) (NP (NNP AP)) (-RRB- -RRB-))) (SBAR (S (NP (CD __) (NNPS Republicans)) (VP (VBP give) (NP (NNP George) (NNP W.) (NNP Bush)) (NP (NP (NN credit)) (PP (IN for) (S (VP (VBG promoting) (NP (DT a) (JJ Russian) (NN role)) (PP (IN in) (S (VP (VBG smoothing) (NP (DT the) (NN transition)) (PP (IN from) (NP (NN despot))) (PP (TO to) (NP (NP (NN democrat)) (PP (IN in) (NP (NNP Yugoslavia)))))))))))))))) (VP (VBD _) (NP (NP (DT an) (NN idea)) (VP (VBN dismissed) (PP (IN in) (NP (NN debate))) (PP (IN as) (ADJP (JJ risky))) (PP (IN by) (NP (NNP Al) (NNP Gore))))) (SBAR (RB even) (IN as) (S (NP (PRP$ his) (NN boss)) (VP (VBD was) (VP (VBG trying) (S (VP (TO to) (VP (VB get) (S (NP (NNP Moscow)) (VP (TO to) (VP (VB step) (PP (IN in))))))))))))) (. .)))")
 
@@ -43,10 +43,10 @@ def augment_tree(t, sent, article):
     For now, handling only the easy case where all tokens of an entity are dominated by one NP
 
     """
-    for tpl in fake_dict[article][sent]:
-    #for tpl in entity_types[article][sent]:
-        entity_type=fake_dict[article][sent][tpl]
-        #entity_type=entity_types[article][sent][tpl]
+    #for tpl in fake_dict[article][sent]:
+    for tpl in entity_types[article][sent]:
+        #entity_type=fake_dict[article][sent][tpl]
+        entity_type=entity_types[article][sent][tpl]
         _add_entity(t,tpl,entity_type)
 
 def _add_entity(t,tpl,entity_type):
@@ -57,7 +57,7 @@ def _add_entity(t,tpl,entity_type):
     parent_positions=[]
     parents=[]
 
-    first_parent_position=t.leaf_treeposition(tpl[0])[:-1]
+    first_parent_position=t.leaf_treeposition(tpl[0])[:-1] #need to change this; first parent should be the parent of a regular word
     first_grandparent_position=first_parent_position[:-1]
 
     for i in range(tpl[0],tpl[-1]):
@@ -93,29 +93,29 @@ def _is_regular_word(t,index):
 
 if __name__ == "__main__":
 
-    augment_tree(test_tree, 3, 'APW20001007.0339.0149')
+    """augment_tree(test_tree, 3, 'APW20001007.0339.0149')
     augment_tree(test_tree2, 4, 'APW20001007.0339.0149')
     print test_tree
     print
     print "************************NEXT SENTENCE************************"
     print
     print test_tree2
-    #print test_tree.__repr__()
-    #test_tree2.draw()
+    #test_tree2.draw()"""
 
-    """outfile=open('test_tree_converter.txt','w')
-    z=0
+    outfile=open('test_tree_converter.txt','w')
+
     for article in entity_types:
+        #print article
         for sent_id in entity_types[article]:
             sent_id=int(sent_id)
             test_tree=NONPARENTED_SENTENCES[article][sent_id+1]
-            augment_tree(test_tree,sent_id+1,article)
-            outfile.write(test_tree.pprint())
+            test_tree_copy=test_tree.copy(deep=True)
+            #print test_tree_copy
+            augment_tree(test_tree_copy,sent_id,article)
+            outfile.write(test_tree_copy.pprint())
             outfile.write('\n\n')
-            print 'boo ',z
-            z+=1
 
-    outfile.close()"""
+    outfile.close()
 
 
 
