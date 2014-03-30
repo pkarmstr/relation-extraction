2a4d3c5f4f560cd1d605554f176e4d654ddcd914
Julia Bosque <jbosgil@googlemail.com>
2014-03-30 12:28:00 -0400

    Added docstrings, and some feature functions. Added _get_mentions_in_order_  to be used throughout the whole file

--- feature_functions.py
+++ feature_functions.py
@@ -30,34 +30,154 @@ def relation_type(fr):
 # Julia's functions #
 #####################
 
+
+def i_token(fr):
+    """return the i_token"""
+    return "i_token={}".format(fr.i_token)
+
+def j_token(fr):
+    """ return the j_token"""
+    return "j_token={}".format(fr.j_token)
+
+def i_entity_type(fr):
+    """return i_entity type"""
+    return "i_entity_type={}".format(fr.i_entity_type)
+
+def j_entity_type(fr):
+    """return j_i_entity_type"""
+    return "j_entity_type={}".format(fr.i_entity_type)
+
+def _get_mentions_in_order_(fr):
+    """return a pair of tuples. The first one corresponds to mention1 and its info,
+    the second one to mention2 and its info. i = mention1 and j=mention2 don't always hold"""
+    if int(fr.i_offset_begin)<int(fr.j_offset_begin):
+        mention1 = (fr.i_token,fr.i_offset_begin,fr.i_offset_end, fr.i_entity_type, fr.i_sentence)
+        mention2 = (fr.j_token,fr.j_offset_begin,fr.j_offset_end, fr.j_entity_type,fr.j_sentence)
+    else:
+        mention2 = (fr.i_token,fr.i_offset_begin,fr.i_offset_end, fr.i_entity_type, fr.i_sentence)
+        mention1 = (fr.j_token,fr.j_offset_begin,fr.j_offset_end, fr.j_entity_type, fr.j_sentence)
+    return (mention1,mention2)
+
+def bow_mention1(fr):
+    """return the words in mention2 eg. [George,Bush]"""
+    mention1 = _get_mentions_in_order_(fr)[0]
+    sent=POS_SENTENCES[fr.article][int(mention1[4])]
+    mention_token = mention1[0]
+    return "bow_mention1={}".format(mention_token.split("_"))
+
+def bow_mention2(fr):
+    """return the words in mention2"""
+    mention2 = _get_mentions_in_order_(fr)[1]
+    sent=POS_SENTENCES[fr.article][int(mention2[4])]
+    mention_token = mention2[0]
+    return "bow_mention1={}".format(mention_token.split("_"))
+
+def _get_words_in_between_(fr):
+    """return the words between m1 and m2"""
+    sent=POS_SENTENCES[fr.article][int(fr.i_sentence)]
+    mention1 =  _get_mentions_in_order_(fr)[0]
+    mention2 =  _get_mentions_in_order_(fr)[1]
+    first_token_index = int(mention1[2])
+    later_token_index = int(mention2[1])
+    w_in_between = sent[first_token_index:later_token_index]
+    return w_in_between
+
+def first_word_inbetween(fr):
+    """return the first word between m1 and m2"""
+    return "first_word_inbetween={}".format(_get_words_in_between_(fr)[0][0])
+
+def last_word_inbetween(fr):
+    """return the last word between m1 and m2"""
+    words = _get_words_in_between_(fr)
+    return "last_word_inbetween={}".format(words[len(words)-1][0])
+
+def other_words_inbetween(fr):
+    """return words between m1 and m2 excluding the first and last words"""
+    words = _get_words_in_between_(fr)
+    words.pop(0)
+    words.pop()
+    return "other_words_inbetween={}".format([w for w, pos in words])
+
+def first_word_before_m1(fr):
+    """return first word before m1"""
+    mention1 = _get_mentions_in_order_(fr)[0]
+    sent=POS_SENTENCES[fr.article][int(mention1[4])]
+    return "first_word_before_m1={}".format(sent[int(mention1[1])-1][0])
+
+
+def first_word_before_m2(fr):
+    """return first word before m2"""
+    mention2 = _get_mentions_in_order_(fr)[1]
+    sent=POS_SENTENCES[fr.article][int(mention2[4])]
+    return "first_word_before_m2={}".format(sent[int(mention2[1])-1][0])
+
+def second_word_before_m1(fr):
+    """return second word before m1"""
+    mention1 = _get_mentions_in_order_(fr)[0]
+    sent=POS_SENTENCES[fr.article][int(mention1[4])]
+    try:
+        return "second_word_before_m1={}".format(sent[int(mention1[1])-2][0])
+    except:
+        IndexError
+        return "second_word_before_m1=NONE"
+
+def second_word_before_m2(fr):
+    """return second word before m2"""
+    mention2 = _get_mentions_in_order_(fr)[1]
+    sent=POS_SENTENCES[fr.article][int(mention2[4])]
+    try:
+        return "second_word_before_m2={}".format(sent[int(mention2[1])-2][0])
+    except:
+        IndexError
+        return "second_word_before_m2=NONE"
+
+
+
 def path_enclosed_tree(fr):
-    """****MONSTER FUNCTION!!!!****"""
+    """****MONSTER FUNCTION!!!!****
+    Return the path enclosed tree between m1 and m2 as PatentedTree
+    The path enclosed tree is the smallest common
+    sub-tree including the two entities, but not necessary the lowest_common_ancestor. In other
+    words, the sub-tree is enclosed by the shortest
+    path linking the two entities in the parse tree (this
+    path is also commonly-used as the path tree feature
+i   n the feature-based methods).
+
+    That is, the path enclosed tree includes mention1, and every branch to the right of it, until
+    mention2. In this function, the path enclosed tree built in the following way:
+    the left branch of it includes mention1 and branches to right of it that still are on the left child
+    of the lowest common ancestor. The right branch of the path-enclosed tree includes mention2, and the
+    branches to the right of it that are on the right child of the lowest common ancestor.
+    Both branches are merged in one tree, with the lowest_common_ancestor node, yielding the
+    path eclosed tree.
+    """
+
 
     if fr.i_sentence!=fr.j_sentence:
         return "Not in same sentence" #just in case
     else:
         #find lowest common ancestor
         s_tree = SYNTAX_PARSE_SENTENCES[fr.article][int(fr.i_sentence)]
+
         ##testing Anya's augmented trees:
         #augtree1="(ROOT (S (S (S (NP (E-PER (NNP Michele) (NNP Roy))) (VP (VBD was) (RB not) (VP (VBN hurt) (PP (IN during) (NP (NP (DT the) (NN dispute)) (PP (IN at) (NP (PRP$ their) (NN home))) (NP-TMP (RB early) (NNP Sunday))))))) (, ,) (CC but) (S (NP (NNP Roy)) (VP (VP (VBD admitted) (S (VP (VBG pulling) (NP (DT a) (NN bedroom) (NN door)) (PP (IN off) (NP (NP (PRP$ its) (NNS hinges))(CC and) (JJ damaging) (NP (DT another)))) (PP (IN after) (NP (NP (PRP$ his) (NN wife)) (VP (VBN called) (NP (E-ORG (NNP Greenwood) (NNP Village) (NN police))))))))) (CC and) (VP (VBD hung) (PRT (RP up)) (PP (IN without) (NP (NN speaking))))))) (, ,) (NP (DT the) (NN report)) (VP (VBD said)) (. .)))"
         #augtree2 ="(ROOT (S (NP (NP (NP (E-GPE (NNP WASHINGTON))) (PRN (-LRB- -LRB-) (NP (NNP AP)) (-RRB- -RRB-))) (SBAR (S (NP (CD __) (NNPS Republicans)) (VP (VBP give) (NP (E-PER (NNP George) (NNP W.) (NNP Bush))) (NP (NP (NN credit)) (PP (S (VP (VBG promoting) (NP (DT a) (JJ Russian) (NN role)) (PP (IN in) (S (VP (VBG smoothing) (NP (DT the) (NN transition)) (PP (IN from) (NP (NN despot))) (PP(TO to) (NP (NP (NN democrat)) (PP (IN in) (NP (NNP Yugoslavia)))))))))))))))) (VP (VBD _) (NP (NP (DT an) (NN idea)) (VP (VBN dismissed) (PP (IN in) (NP (NN debate))) (PP (IN as) (ADJP (JJ risky))) (PP (IN by) (NP (NNP Al) (NNP Gore))))) (SBAR (RB even) (IN as) (S (NP (PRP$ his) (NN boss)) (VP (VBD was) (VP (VBG trying) (S (VP (TO to) (VP (VB get) (S (NP (NNP Moscow)) (VP (TO to) (VP (VB step) (PP (IN in))))))))))))) (. .)))"
-        #s_tree = ParentedTree.parse(augtree1)
+        #augtree3 = "(ROOT (NP (NP (NP (E-GPE (NNP KABUL)) (, ,) (E-GPE (NNP Afghanistan))) (PRN (-LRB- -LRB-) (NP (NNP AP)) (-RRB- -RRB-))) (NP (NP (CD _)) (SBAR (S (NP (NP (DT The) (NN ruling) (E-ORG (NNP Taliban)) (E-ORG (NN militia))) (PP (IN on) (NP (NNP Monday)))) (VP (VP (VBD released) (NP (CD 137) (E-PER (JJ Shiite)) (E-PER (JJ Muslim)) (E-PER (NNS prisoners))) (SBAR (S (NP (E-ORG (PRP it))) (VP (VBD had) (VP (VBN held) (PP (IN for) (NP (QP (RB nearly) (CD two)) (NNS years)))))))) (CC and) (VP (VBD urged) (NP (DT the) (E-ORG (NN opposition))) (S (VP (TO to) (VP (VB follow) (NP (NP (NP (NN suit) (CC and) (NN release)) (E-ORG (NN government)) (E-PER (NNS prisoners))) (SBAR (S (NP (E-ORG (PRP it))) (VP (VBZ is) (VP (VBG holding)))))))))))))) (. .)))"
+        #augtree4= "(ROOT (S (NP (DT The) (VBN freed) (E-PER (NNS men))) (PRN (, ,) (S (NP (DT all)) (VP (VBD said) (S (VP (TO to) (VP (VB be) (NP (NP (E-PER (NNS fighters))) (VP (VBG belonging) (PP (TO to) (NP (DT the) (E-ORG (NN opposition)) (E-ORG (NN alliance))))))))))) (, ,)) (VP (VBD were) (VP (VBN released) (ADVP (RB ahead)) (PP (IN of) (NP (NP (DT the) (E-PER (JJ Islamic)) (JJ holy) (NN month)) (PP (IN of) (NP (NNP Ramadan))))) (, ,) (SBAR (WHADVP (WRB when)) (S (NP (JJ devout) (E-PER (NNPS Muslims))) (ADVP (RB fast) (PP (IN from) (NP (NN sunrise)))) (VP (TO to) (VP (VB sunset))))))) (. .)))"
+        #augtree7="(ROOT (S (NP (NP (DT The) (E-ORG (NN opposition)) (E-ORG (NN alliance))) (, ,) (SBAR (WHNP (E-ORG (WDT which))) (S (VP (VP (VBZ controls) (NP (NP (QP (RB barely) (CD five)) (NN percent)) (PP (IN of) (NP (E-GPE (NNP Afghanistan)))))) (CC and) (VP (VBZ is) (VP (VBG fighting) (NP (NP (DT a) (NN war)) (PP (IN against) (NP (DT the) (JJ dominant) (E-ORG (NNP Taliban)))))))))) (, ,)) (VP (VBZ is) (VP (ADVP (RB mostly)) (VBN made) (ADVP (IN up) (PP (IN of) (NP (NP (DT the) (E-GPE (NN country)) (POS 's)) (NN minority)))) (NP (ADJP (JJ ethnic) (CC and) (JJ religious)) (E-PER (NNS groups))))) (. .)))"
+        #augtree33="(ROOT (SBARQ (SBAR (NP (NN __)) (IN If) (S (NP (E-ORG (NNP CBS))) (VP (VBZ shows) (NP (NP (DT the) (JJ first) (NN episode)) (PP (IN of) (`` ``) (NP (NNP Survivor) (NNP II)) ('' ''))) (PP (IN after) (NP (NP (E-ORG (PRP$ its)) (NN broadcast)) (PP (IN of) (NP (NNP Super) (NNP Bowl) (NNP XXXV)))))))) (, ,) (SQ (MD will) (NP (DT the) (E-ORG (NN network))) (VP (ADVP (RB someday)) (VBP show) (NP (NP (DT the) (JJ first) (NN episode)) (PP (IN of) (NP (`` ``) (NP (NNP Survivor) (NNP XXXV)) ('' '') (PP (IN after) (NP (NP (E-ORG (PRP$ its)) (NN broadcast)) (PP (IN of) (NP (NNP Super) (NNP Bowl) (NNP LXVIII)))))))))) (. ?)))"
+        #augtree34="(ROOT (S (NP (NN __)) (VP (MD Will) (S (ADJP (JJ cheerful)) (SBAR (S (NP (NP (NN chorus) (E-PER (NNS members))) (VP (VBN dressed) (PP (IN in) (NP (JJ V-neck) (NNS sweaters))))) (VP (VBP interrupt) (NP (E-PER (PRP$ their)) (NNS serenades)) (PP (TO to) (NP (JJ online) (NN shopping))) (ADVP (RB long) (RB enough) (S (VP (TO to) (VP (VB tell) (NP (NP (DT an) (NN advertising) (E-PER (NN columnist))) (, ,) (S (`` ``) (NP (PRP You)) (VP (VBP ask) (NP (NP (DT a) (NN lot)) (PP (IN of) (NP (NP (NNS questions)) (PP (IN for) (NP (E-PER (NN someone))))))) (PP (IN from) (NP (E-GPE (NNP Brooklyn))))) ('' '')))))))))))) (. ?)))"
+        #s_tree = ParentedTree.parse(augtree4)
         #print "printing leaves corresponding to indices"
         #print s_tree.leaves()[int(fr.i_offset_begin)] #checking indices first...
         #print s_tree.leaves()[int(fr.j_offset_begin)]
 
-        if int(fr.j_offset_begin)>int(fr.i_offset_begin):
-            first_entity_index = int(fr.i_offset_begin)
-            later_entity_index = int(fr.j_offset_end)-1
-            first_token = fr.i_token
-            later_token = fr.j_token
-
-        else:
-            first_entity_index = int(fr.j_offset_begin)
-            later_entity_index = int(fr.i_offset_end)-1
-            first_token = fr.j_token
-            later_token = fr.i_token
-
+        mention1 = _get_mentions_in_order_(fr)[0]
+        mention2= _get_mentions_in_order_(fr)[1]
+        first_entity_index = int(mention1[1])
+        later_entity_index = int(mention2[2])-1
+        first_token = mention1[0]
+        later_token = mention2[0]
         i_tuple = s_tree.leaf_treeposition(first_entity_index)
         j_tuple = s_tree.leaf_treeposition(later_entity_index)
         #print "printing indices"
--- tester.py
+++ tester.py
@@ -22,34 +22,86 @@ class RelTester(unittest.TestCase):
     ##JULIA'S TESTS##
     #################
 
-    def test_path_enclosed_tree(self):
-        ####doing some tests
-        line1 ="EMP-ORG.Member-of-Group.reverse NYT20001019.2136.0319 12 4 5 ORG Republican 12 6 7 PER candidate Republican candidate".rstrip().split()
-        line2 = "no_rel NYT20001019.2136.0319 12 4 5 ORG Republican 12 19 20 GPE Yemen Republican Yemen".rstrip().split()
-        line3 = "PHYS.Located NYT20001019.2136.0319 25 14 15 FAC preserve 25 16 17 GPE Alaska preserve Alaska".rstrip().split()
-        line4 = "ART.User-or-Owner NYT20001019.2136.0319 27 10 12 GPE ``_we 27 16 17 VEH cars ``_we cars".rstrip().split()
-        line5 = "no_rel NYT20001020.2025.0304 14 6 7 GPE Palestinian 14 19 20 PER terrorists Palestinian terrorists".rstrip().split()
-        line6 = "PHYS.Located NYT20001017.1908.0279 7 4 6 PER Murray_Schwartz 7 7 8 GPE Wilmington Murray_Schwartz Wilmington".rstrip().split()
-        line7 = "EMP-ORG.Subsidiary.reverse NYT20001017.1908.0279 8 9 10 ORG which 8 12 13 ORG company which company".rstrip().split()
-        line8 = "no_rel NYT20001017.1908.0279 9 4 5 ORG many 9 47 48 FAC headquarters many headquarters".rstrip().split()
-        line9 = "no_rel NYT20001017.1908.0279 9 9 10 ORG companies 9 29 30 GPE Delaware companies Delaware".rstrip().split()
-        ##testing Anya's augmented trees
-        line10 = "no_rel APW20001007.0339.0149 3 25 26 GPE Yugoslavia 3 35 37 PER Al_Gore Yugoslavia Al_Gore".rstrip().split()
-        line11 = "no_rel APW20001023.2100.0686 5 9 10 PER their 5 31 34 ORG Greenwood_Village_police their Greenwood_Village_police".rstrip().split()
-        line12 = "no_rel APW20001023.2100.0686 5 0 2 PER Michele_Roy 5 19 20 FAC bedroom Michele_Roy bedroom".rstrip().split()
+    def test_bow_mentions(self):
+        line1 ="PHYS.Located NYT20001017.1908.0279 7 4 6 PER Murray_Schwartz 7 7 8 GPE Wilmington Murray_Schwartz Wilmington".rstrip().split()
         fr1 = FeatureRow(*line1)
-        fr2 = FeatureRow(*line2)
-        fr3 = FeatureRow(*line3)
-        fr4 = FeatureRow(*line4)
-        fr5 = FeatureRow(*line5)
-        fr6 = FeatureRow(*line6)
-        fr7 = FeatureRow(*line7)
-        fr8 = FeatureRow(*line8)
-        fr9 = FeatureRow(*line9)
-        fr10 = FeatureRow(*line10)
-        fr11 = FeatureRow(*line11)
-        fr12 = FeatureRow(*line12)
-        path_enclosed_tree(fr9).draw()
+
+    def test_first_word_inbetween(self):
+        line1 ="PHYS.Located NYT20001017.1908.0279 7 4 6 PER Murray_Schwartz 7 7 8 GPE Wilmington Murray_Schwartz Wilmington".rstrip().split()
+        fr1 = FeatureRow(*line1)
+        self.assertTrue(first_word_inbetween(fr1).endswith("in"))
+
+    def test_last_word_inbetween(self):
+        line1 = "no_rel NYT20001019.2136.0319 12 4 5 ORG Republican 12 19 20 GPE Yemen Republican Yemen".rstrip().split()
+        fr1 = FeatureRow(*line1)
+        self.assertTrue(last_word_inbetween(fr1).endswith("in"))
+
+    def test_other_words_inbetween(self):
+        line1 = "no_rel NYT20001019.2136.0319 12 4 5 ORG Republican 12 19 20 GPE Yemen Republican Yemen".rstrip().split()
+        fr1 = FeatureRow(*line1)
+        self.assertTrue(other_words_inbetween(fr1).split("=")[1] ==
+                        "[u'candidate', u'on', u'the', u'death', u'penalty', u',', u'the', u'bombing', u'of', u'a', u'Navy', u'ship']")
+
+    def test_first_word_before_m1(self):
+        line1 = "no_rel NYT20001019.2136.0319 12 4 5 ORG Republican 12 19 20 GPE Yemen Republican Yemen".rstrip().split()
+        fr1 = FeatureRow(*line1)
+        self.assertTrue(first_word_before_m1(fr1).endswith("the"))
+
+    def test_first_word_before_m2(self):
+        line1 = "no_rel NYT20001019.2136.0319 12 4 5 ORG Republican 12 19 20 GPE Yemen Republican Yemen".rstrip().split()
+        fr1 = FeatureRow(*line1)
+        self.assertTrue(first_word_before_m2(fr1).endswith("in"))
+
+    def test_second_word_before_m1(self):
+        line1 = "no_rel NYT20001019.2136.0319 12 4 5 ORG Republican 12 19 20 GPE Yemen Republican Yemen".rstrip().split()
+        fr1 = FeatureRow(*line1)
+        self.assertTrue(second_word_before_m1(fr1).endswith("grilled"))
+
+    def test_second_word_before_m2(self):
+        line1 = "no_rel NYT20001019.2136.0319 12 4 5 ORG Republican 12 19 20 GPE Yemen Republican Yemen".rstrip().split()
+        fr1 = FeatureRow(*line1)
+        self.assertTrue(second_word_before_m2(fr1).endswith("ship"))
+
+
+    #def test_path_enclosed_tree(self):
+        ####doing some tests
+        #line1 ="EMP-ORG.Member-of-Group.reverse NYT20001019.2136.0319 12 4 5 ORG Republican 12 6 7 PER candidate Republican candidate".rstrip().split()
+        #line2 = "no_rel NYT20001019.2136.0319 12 4 5 ORG Republican 12 19 20 GPE Yemen Republican Yemen".rstrip().split()
+        #line3 = "PHYS.Located NYT20001019.2136.0319 25 14 15 FAC preserve 25 16 17 GPE Alaska preserve Alaska".rstrip().split()
+        #line4 = "ART.User-or-Owner NYT20001019.2136.0319 27 10 12 GPE ``_we 27 16 17 VEH cars ``_we cars".rstrip().split()
+        #line5 = "no_rel NYT20001020.2025.0304 14 6 7 GPE Palestinian 14 19 20 PER terrorists Palestinian terrorists".rstrip().split()
+        #line6 = "PHYS.Located NYT20001017.1908.0279 7 4 6 PER Murray_Schwartz 7 7 8 GPE Wilmington Murray_Schwartz Wilmington".rstrip().split()
+        #line7 = "EMP-ORG.Subsidiary.reverse NYT20001017.1908.0279 8 9 10 ORG which 8 12 13 ORG company which company".rstrip().split()
+        #line8 = "no_rel NYT20001017.1908.0279 9 4 5 ORG many 9 47 48 FAC headquarters many headquarters".rstrip().split()
+        #line9 = "no_rel NYT20001017.1908.0279 9 9 10 ORG companies 9 29 30 GPE Delaware companies Delaware".rstrip().split()
+        ###testing Anya's augmented trees
+        #line10 = "no_rel APW20001007.0339.0149 3 0 1 GPE WASHINGTON 3 7 10 PER George_W._Bush WASHINGTON George_W._Bush".rstrip().split()
+        #line11 = "no_rel APW20001023.2100.0686 5 9 10 PER their 5 31 34 ORG Greenwood_Village_police their Greenwood_Village_police".rstrip().split()
+        #line12 = "no_rel APW20001023.2100.0686 5 0 2 PER Michele_Roy 5 19 20 FAC bedroom Michele_Roy bedroom".rstrip().split()
+        #line3a="no_rel APW20001120.1450.0376 3 9 10 ORG Taliban 3 15 16 PER Shiite Taliban Shiite".rstrip().split()
+        #line4a="no_rel APW20001120.1450.0376 4 20 21 PER Islamic 4 28 29 PER Muslims Islamic Muslims".rstrip().split()
+        #line7a = "GPE-AFF.Citizen-or-Resident.reverse APW20001120.1450.0376 7 27 28 GPE country 7 33 34 PER groups country groups".rstrip().split()
+        #line33a="no_rel NYT20001123.1511.0062 33 22 23 ORG network 33 34 35 ORG its network its".rstrip().split()
+        #line34a="no_rel NYT20001123.1511.0062 34 21 22 PER columnist 34 23 25 PER ``_You columnist ``_You".rstrip().split()
+
+        #fr1 = FeatureRow(*line1)
+        #fr2 = FeatureRow(*line2)
+        #fr3 = FeatureRow(*line3)
+        #fr4 = FeatureRow(*line4)
+        #fr5 = FeatureRow(*line5)
+        #fr6 = FeatureRow(*line6)
+        #fr7 = FeatureRow(*line7)
+        #fr8 = FeatureRow(*line8)
+        #fr9 = FeatureRow(*line9)
+        #fr10 = FeatureRow(*line10)
+        #fr11 = FeatureRow(*line11)
+        #fr12 = FeatureRow(*line12)
+        #fr3a=FeatureRow(*line3a)
+        #fr4a=FeatureRow(*line4a)
+        #fr7a=FeatureRow(*line7a)
+        #fr33a=FeatureRow(*line33a)
+        #fr34a=FeatureRow(*line34a)
+        #path_enclosed_tree(fr5).draw()
 
 
 
