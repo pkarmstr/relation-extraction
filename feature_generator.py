5c5a688421ec9ea5579783df3fa0ca9549075f2f
pkarmstr <pkarmstr@gmail.com>
2014-04-04 02:02:33 -0400

    fixed tree svm thing

--- feature_generator.py
+++ feature_generator.py
@@ -37,13 +37,14 @@ class Featurizer:
     def build_features(self):
         self.new_features = []
         for feats in self.original_data:
-            new_row = []
-            if not self.no_tag:
-                new_row.append(feats.relation_type)
-            for func in self.tree_functions:
-                new_row.append("|BT|")
-                new_row.append(func(feats)._pprint_flat('', '()', False))
-            new_row.append("|ET|")
+            if self.tree_functions:
+                new_row = []
+                if not self.no_tag:
+                    new_row.append(feats.relation_type)
+                for func in self.tree_functions:
+                    new_row.append("|BT|")
+                    new_row.append(func(feats)._pprint_flat('', '()', False))
+                new_row.append("|ET|")
             if self.feature_functions:
                 for i,func in self.feature_functions:
                     cell = func(feats)
